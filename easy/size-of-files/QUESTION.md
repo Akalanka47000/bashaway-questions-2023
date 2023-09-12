0c0c521d28013f45055afd998821529baf348416
Akalanka <akalankaperera128@gmail.com>
2023-09-12 21:09:32 +0530

    Feat: added byte master

--- easy/size-of-files/QUESTION.md
+++
@@ -1,5 +0,0 @@
-# Size of Files
-
-Give set of files and get sum of the file sizes in bytes
-
-Like few text, image and psd project files in a folder and get the total size of the folder in bytes.
--- medium/cluttered/code/execute.sh
+++ medium/cluttered/code/execute.sh
@@ -2,4 +2,6 @@ docker pull mongo:latest
 
 docker run -d --name mongodb -p 27207:27017 mongo
 
+# Data will be populated into the above database instance once the tests are run.
+
 # Write your code here
